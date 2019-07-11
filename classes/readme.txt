9b5731a0a38e2d27fd77dd29552079345c81da4c
Abhijit Babasaheb Nage <abhijitn@bsf.io>
2019-07-11 12:26:36 +0530

    added text field in general setting for setting up the frequency and added table for shortcode information in getting a started tab in plugin setting page.This Plugin helps to fetch every information of Astra theme using shortcodes

---
+++ classes/readme.txt
@@ -0,0 +1,36 @@
+=== WP Themes Active Stats ===
+Contributors: brainstormforce 
+Tags: wpthemes-count, active-install, stats, themes-stats    
+Requires at least: 3.0  
+Tested up to: 5.1    
+Stable tag: 1.0.0  
+
+== Description ==
+
+This plugin is geared towards developers with Themes in the WordPress.org repository and anyone else that wants to easily display information about a Themes that is in the repository.
+
+Here is a shortcodes available in the WP Themes Active Stats plugin.
+
+`[wp_theme_active_install wp_theme_slug='astra' theme_author='brainstormforce']`
+
+1.`wp_theme_slug="astra"`
+
+	Name of the Theme from w.org that you want get active install count.
+
+2.`theme_author="brainstormforce"`
+
+	Theme Author name.
+
+== Installation ==
+
+1. Unzip the downloaded zip file
+2. Upload the included folder to '/wp-content/plugins' directory of your WordPress installation
+3. Activate the plugin via the WordPress Plugins page
+
+== Frequently Asked Questions ==
+
+1. How it works? =
+It's a shortcode. Add [wp_theme_active_install wp_theme_slug='astra' theme_author='brainstormforce'] to a page/post.
+
+== Version 1.0.0 ==
+* Initial release
---
+++ includes/wpas-user-manual.php
@@ -0,0 +1,72 @@
+<?php
+wp_enqueue_style('bsf_wpas_as_stylesheet');
+	?>
+		<div class="bsfresp-table">
+	   			<div class="bsfresp-table-caption">THEME SHORTCODE TABLE</div>
+					<div class="bsfresp-table-header">
+						<div class="bsftable-header-cell">
+							Total active installs
+						</div>
+						<div class="bsftable-body-cell">
+							[wp_theme_active_install wp_theme_slug='astra']
+						</div>
+					</div>
+					<div class="bsfresp-table-header">
+						<div class="bsftable-header-cell">
+							Last Updated
+						</div>
+						<div class="bsftable-body-cell">
+							[wp_theme_last_updated wp_theme_slug='astra']
+						</div>
+					</div>
+					<div class="bsfresp-table-header">
+						<div class="bsftable-header-cell">
+							Theme Version
+						</div>
+						<div class="bsftable-body-cell">
+							[wp_theme_version wp_theme_slug='astra']
+						</div>
+					</div>
+					<div class="bsfresp-table-header">
+						<div class="bsftable-header-cell">
+							Theme Ratings
+						</div>
+						<div class="bsftable-body-cell">
+							[wp_theme_ratings wp_theme_slug='astra']
+						</div>
+					</div>
+					<div class="bsfresp-table-header">
+						<div class="bsftable-header-cell">
+							5 Star Ratings
+						</div>
+						<div class="bsftable-body-cell">
+							[wp_theme_5starRate wp_theme_slug='astra']
+						</div>
+					</div>
+					<div class="bsfresp-table-header">
+						<div class="bsftable-header-cell">
+							Average Ratings
+						</div>
+						<div class="bsftable-body-cell">
+							[wp_theme_averageRate wp_theme_slug='astra']
+						</div>
+					</div>
+					<div class="bsfresp-table-header">
+						<div class="bsftable-header-cell">
+							Total Downloads
+						</div>
+						<div class="bsftable-body-cell">
+							[wp_theme_total_downloads wp_theme_slug='astra']
+						</div>
+					</div>
+					<div class="bsfresp-table-header">
+						<div class="bsftable-header-cell">
+							 Download
+						</div>
+						<div class="bsftable-body-cell">
+							 [wp_theme_downloadlink wp_theme_slug='astra']
+						</div>
+					</div>
+				</div>
+	<?php
+?>
